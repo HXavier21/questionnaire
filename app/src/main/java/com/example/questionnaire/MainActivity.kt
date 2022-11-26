0c9ddba30bacd675bb2940778868b516ba1f2ac1
Xavier <1585375420@qq.com>
2022-11-26 11:33:47 +0800

    NavigateAttempt

--- app/src/main/java/com/example/questionnaire/MainActivity.kt
+++ app/src/main/java/com/example/questionnaire/MainActivity.kt
@@ -1,50 +1,31 @@
 package com.example.questionnaire
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.questionnaire.ui.SampleData
-import com.example.questionnaire.ui.theme.QuestionnaireTheme
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(msg = Message("Google", "Android"))
+            HomeScreen(onNavigateToCreate = {}, onNavigateToImport = {})
+
         }
     }
 }
@@ -52,89 +33,70 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(modifier: Modifier = Modifier, msg: Message) {
-    Row(
-        modifier = modifier
-            .padding(all = 8.dp)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
+fun MyAppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "HomeScreen"
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        var isExpanded by remember { mutableStateOf(false) }
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-        )
-        Column(modifier = Modifier) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleMedium
+        composable("HomeScreen") {
+            HomeScreen(
+                onNavigateToCreate = { navController.navigate("CreateScreen") },
+                onNavigateToImport = { navController.navigate("ImportScreen") }
             )
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(
-                color = surfaceColor,
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)
-                    .clip(MaterialTheme.shapes.medium)
-                    .clickable { isExpanded = !isExpanded }
-                    ,
-//                shape = MaterialTheme.shapes.medium,
-                tonalElevation = 1.dp
-            ) {
+        }
+        composable("CreateScreen") { CreateScreen(/*...*/) }
+        composable("ImportScreen") {}
+    }
+}
+
+@Composable
+fun HomeScreen(onNavigateToCreate: () -> Unit, onNavigateToImport: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.weight(1f)) {}
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onNavigateToImport
+                ) {
+                    Text(
+                        text = "Import Questionnaire",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
                 Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
+                    text = "or",
+                    color = MaterialTheme.colorScheme.secondaryContainer,
+                    style = MaterialTheme.typography.titleMedium
                 )
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onNavigateToCreate
+                ) {
+                    Text(
+                        text = "Create New Questionnaire",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
             }
         }
     }
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    name = "Dark Mode",
-    showBackground = true,
-)
 @Composable
-fun PreviewMessageCard() {
-    QuestionnaireTheme() {
-        Surface(
-            shape = RoundedCornerShape(20.dp)
-        ) {
-            MessageCard(
-                modifier = Modifier.clickable {},
-                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"),
-            )
-        }
-    }
-}
+fun CreateScreen(){
+    Surface(){
 
-@Composable
-fun Conversation(message: List<Message>) {
-    LazyColumn() {
-        for (msg in message) {
-            item { MessageCard(msg = msg) }
-        }
-//        message.map{item { MessageCard(msg = it) }}
     }
 }
 
 @Preview
 @Composable
-fun PreviewConversation() {
-    QuestionnaireTheme() {
-        Conversation(message = SampleData.conversationSample)
-    }
+fun PreviewHome() {
+    HomeScreen(onNavigateToCreate = {}, onNavigateToImport = {})
 }
