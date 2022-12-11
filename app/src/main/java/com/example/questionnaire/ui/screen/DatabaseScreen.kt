1c3f5def90c440fd79562f37e32abba0d1fe02af
Xavier <hxavierhugo@gmail.com>
2022-12-11 17:26:08 +0800

    final

--- app/src/main/java/com/example/questionnaire/ui/screen/DatabaseScreen.kt
+++ app/src/main/java/com/example/questionnaire/ui/screen/DatabaseScreen.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -90,24 +92,26 @@ fun DatabaseScreen(
         { popUpTo(RouteName.HOME_SCREEN) { inclusive = true } }
     }
     QuestionnaireTheme {
-        Column() {
-            CustomText(
-                text = "Questionnaire Database",
-                style = MaterialTheme.typography.displaySmall,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(all = 20.dp)
-            )
-            LazyColumn(verticalArrangement = Arrangement.spacedBy(15.dp)) {
-                for (questionnaire in questionnairelist) {
-                    item {
-                        DatabaseItem(
-                            topic = questionnaire.topic,
-                            json = questionnaire.json,
-                            quizViewModel = quizViewModel,
-                            navController = navController
-                        )
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column() {
+                CustomText(
+                    text = "Questionnaire Database",
+                    style = MaterialTheme.typography.displaySmall,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(all = 20.dp)
+                )
+                LazyColumn(verticalArrangement = Arrangement.spacedBy(15.dp)) {
+                    for (questionnaire in questionnairelist) {
+                        item {
+                            DatabaseItem(
+                                topic = questionnaire.topic,
+                                json = questionnaire.json,
+                                quizViewModel = quizViewModel,
+                                navController = navController
+                            )
+                        }
+                        Log.d(TAG, "topic:" + questionnaire.topic + "\njson:" + questionnaire.json)
                     }
-                    Log.d(TAG, "topic:" + questionnaire.topic + "\njson:" + questionnaire.json)
                 }
             }
         }
